79c979f6cd8ddb443f4894ceeba2b75ed7e20bd8
Dhaesha M <dhaesha1@gmail.com>
2022-03-26 01:27:01 -0400

    app runnin w no errors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "./index.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>React Router</h1>
+    </div>
+  );
 }
 
 export default App;
