d2b2448a490e7f1ce48b07e0a33ae1d673dc67a4
Dhaesha M <dhaesha1@gmail.com>
2022-03-27 17:38:43 -0400

    starting hero component

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+  FaDatabase,
+  FaAsterisk,
+  FaAccusoft,
+  FaFacebook,
+  FaTwitter,
+  FaGithub,
+} from "react-icons/fa";
+import "./hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="container"></div>
+    </div>
+  );
+};
+
+export default Hero;
