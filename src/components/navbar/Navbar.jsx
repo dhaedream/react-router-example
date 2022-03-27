c62926180ab8e80e94d004adfbe09989652aa102
Dhaesha M <dhaesha1@gmail.com>
2022-03-26 21:08:43 -0400

    added logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import "./navbar.css";
 import { Link } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
+import logo from "../../assets/logo.png";
 
 const Navbar = () => {
   return (
     <div>
       <header>
         <nav className="navbar">
-          <div className="logo"></div>
+          <div className="logo">
+            <img src={logo} alt="logo" />
+          </div>
         </nav>
       </header>
     </div>
