69b9bf1e074a888f00760c995b5b6e787657ee26
Dhaesha M <dhaesha1@gmail.com>
2022-03-28 11:56:17 -0400

    added all component folders

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../navbar/Navbar";
 import Hero from "../hero/Hero";
 import "./home.css";
+import Pricing from "../pricing/Pricing";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Pricing />
     </div>
   );
 };
