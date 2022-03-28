9af4fc38f60cba765ed7eaaa6a018ae855399462
Dhaesha M <dhaesha1@gmail.com>
2022-03-27 22:35:59 -0400

    added Hero component to Home

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -39,6 +39,42 @@ const Hero = () => {
               </div>
             </div>
           </div>
+          <div className="col-2">
+            <div className="form-layout">
+              <div className="form-container">
+                <p className="sign-in-txt">Sign in with</p>
+                <div className="social-login">
+                  <i>
+                    <FaFacebook size={20} />
+                  </i>
+                  <i>
+                    <FaTwitter size={20} />
+                  </i>
+                  <i>
+                    <FaGithub size={20} />
+                  </i>
+                </div>
+                <div className="divider">
+                  <p>
+                    <span>Or</span>
+                  </p>
+                </div>
+                <form action="">
+                  <input type="text" placeholder="Name" />
+                  <input type="email" placeholder="Email" />
+                  <input type="password" placeholder="Password" />
+                  <button>Create your account</button>
+                </form>
+              </div>
+              <div className="form-footer">
+                <p>
+                  By signing up, you agree to our
+                  <span className="primary-color"> Terms, Data Policy</span> and
+                  <span className="primary-color"> Cookies Policy</span>.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../navbar/Navbar";
+import Hero from "../hero/Hero";
 import "./home.css";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 };
