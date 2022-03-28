a34c9a78a2c8281e72a74ada346601a65bffc74c
Dhaesha M <dhaesha1@gmail.com>
2022-03-27 22:23:10 -0400

    added icons

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -19,6 +19,25 @@ const Hero = () => {
             <h1>
               <span className="primary-color">online business</span>
             </h1>
+            <p>
+              Chocolate sweet roll gummi bears cheesecake gingerbread cookie
+              pastry tart pie. Fruitcake powder topping bonbon toffee topping I
+              love biscuit. Jelly beans I love cookie donut pie jelly.
+            </p>
+            <div className="used-by">
+              <p>USED BY</p>
+              <div className="icons">
+                <i>
+                  <FaDatabase /> Staxx
+                </i>
+                <i>
+                  <FaAsterisk /> Star AI
+                </i>
+                <i>
+                  <FaAccusoft /> Accusoft
+                </i>
+              </div>
+            </div>
           </div>
         </div>
       </div>
